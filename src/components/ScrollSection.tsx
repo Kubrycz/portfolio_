fed28be82c85a093b9af337d69113d375c23ee6f
paulina.kuberska <p.k.kuberska@gmail.com>
2026-01-19 18:41:50 +0100

    removing unnecessary comments

--- src/components/ScrollSection.tsx
+++ src/components/ScrollSection.tsx
@@ -1,8 +1,6 @@
 const ScrollToSection = () => {
-  console.log("Kliknięto przycisk! ✅");
 
   const section = document.getElementById("scrollAbout-section");
-  console.log("Znaleziony element:", section);
 
   if (section) {
     const yOffset = -150;
@@ -10,7 +8,6 @@ const ScrollToSection = () => {
       section.getBoundingClientRect().top + window.pageYOffset + yOffset;
 
     window.scrollTo({ top: y, behavior: "smooth" });
-    console.log("Scroll");
   } else {
     console.warn("Nie znaleziono sekcji");
   }
